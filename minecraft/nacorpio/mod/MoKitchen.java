9a5d312e8ccec5f51f83d6f1d385c96302b4e584
Nacorpio <teampavilion@gmail.com>
2013-06-20 17:35:42 +0200

    Added most of the properties and names to the registry
    I added a lot of all the properties. I also added most of the names to
    the registry. I changed some of the recipes too.

--- minecraft/nacorpio/mod/MoKitchen.java
+++ minecraft/nacorpio/mod/MoKitchen.java
@@ -2,6 +2,7 @@ package nacorpio.mod; //Package directory
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.EnumToolMaterial;
 import net.minecraft.item.Item;
@@ -69,23 +70,69 @@ public class MoKitchen {
 	
 	
 	// Accessories (Spices: Can be used on different meals) 
+	public static Item accessoryPlate;
+	public static Item accessoryWineGlass;
+	public static Item accessoryGlass;
+	
 	public static Item accessoryChiliSpice;
 	public static Item accessoryOnionSpice;
 	public static Item accessoryBeefSpice;
 	
+	public static Item accessoryCheeseSlice;
 	public static Item accessoryButter; // TODO: This is used on sandwiches.
 	
 @Init
 	public void load(FMLInitializationEvent event){
 
-	foodDonut = (ItemFood) new ItemFood(3337, 3, false).setUnlocalizedName("food_donut");
+	eatableStrawberry = (ItemFood) new ItemFood(3349, 1, false).setUnlocalizedName("strawberry");
+	eatableRaspberry = (ItemFood) new ItemFood(3354, 1, false).setUnlocalizedName("raspberry");
+	eatableOrange = (ItemFood) new ItemFood(3355, 1, false).setUnlocalizedName("orange");
+	eatableLemon = (ItemFood) new ItemFood(3356, 1, false).setUnlocalizedName("lemon");
+	eatableGrape = (ItemFood) new ItemFood(3357, 1, false).setUnlocalizedName("grape");
 	
-	itemCheese = new Item(3338).setUnlocalizedName("item_cheese");
+	foodDonut = (ItemFood) new ItemFood(3337, 3, false).setUnlocalizedName("donut").setCreativeTab(CreativeTabs.tabKitchen);
+	foodPieApple = (ItemFood) new ItemFood(3345, 12, false).setUnlocalizedName("apple_pie").setCreativeTab(CreativeTabs.tabKitchen);
+	foodPieRhubarb = (ItemFood) new ItemFood(3346, 12, false).setUnlocalizedName("rhubarb_pie").setCreativeTab(CreativeTabs.tabKitchen);
+	foodOatPorridge = (ItemFood) new ItemFood(3347, 6, false).setUnlocalizedName("oat_porridge").setCreativeTab(CreativeTabs.tabKitchen);
+	foodWheatPorridge = (ItemFood) new ItemFood(3348, 6, false).setUnlocalizedName("wheat_porridge").setCreativeTab(CreativeTabs.tabKitchen);
 	
-	drinkRedWine = (ItemFood) new ItemFood(3339, 1, false).setUnlocalizedName("drink_redWine");
-	drinkWhiteWine = (ItemFood) new ItemFood(3340, 1, false).setUnlocalizedName("drink_whiteWine");
+	itemCheese = new Item(3338).setUnlocalizedName("cheese_full").setCreativeTab(CreativeTabs.tabKitchen);
+	itemMeat = new Item(3341).setUnlocalizedName("meat").setCreativeTab(CreativeTabs.tabKitchen);
+	itemFlour = new Item(3342).setUnlocalizedName("flour").setCreativeTab(CreativeTabs.tabKitchen);
+	itemOat = new Item(3343).setUnlocalizedName("oat").setCreativeTab(CreativeTabs.tabKitchen);
+	itemSalt = new Item(3344).setUnlocalizedName("salt").setCreativeTab(CreativeTabs.tabKitchen);
 	
-	//	LanguageRegistry.addName(???, "???");
+	accessoryChiliSpice = new Item(3349).setUnlocalizedName("chili_spice").setCreativeTab(CreativeTabs.tabKitchen);
+	accessoryOnionSpice = new Item(3350).setUnlocalizedName("onion_spice").setCreativeTab(CreativeTabs.tabKitchen);
+	accessoryBeefSpice = new Item(3351).setUnlocalizedName("beef_spice").setCreativeTab(CreativeTabs.tabKitchen);
+	accessoryCheeseSlice = new Item(3352).setUnlocalizedName("cheese_slice").setCreativeTab(CreativeTabs.tabKitchen);
+	accessoryButter = new Item(3353).setUnlocalizedName("butter").setCreativeTab(CreativeTabs.tabKitchen);
+	
+	drinkRedWine = (ItemFood) new ItemFood(3339, 1, false).setUnlocalizedName("red_wine").setCreativeTab(CreativeTabs.tabKitchen);
+	drinkWhiteWine = (ItemFood) new ItemFood(3340, 1, false).setUnlocalizedName("white_wine").setCreativeTab(CreativeTabs.tabKitchen);
+	
+	LanguageRegistry.addName(eatableStrawberry, "Strawberry");
+	
+	LanguageRegistry.addName(foodDonut, "Donut");
+	LanguageRegistry.addName(foodPieApple, "Apple Pie");
+	LanguageRegistry.addName(foodPieRhubarb, "Rhubarb Pie");
+	LanguageRegistry.addName(foodOatPorridge, "Oat Porridge");
+	LanguageRegistry.addName(foodWheatPorridge, "Wheat Porridge");
+	
+	LanguageRegistry.addName(itemCheese, "Cheese");
+	LanguageRegistry.addName(itemMeat, "Meat");
+	LanguageRegistry.addName(itemFlour, "Flour");
+	LanguageRegistry.addName(itemOat, "Oat");
+	LanguageRegistry.addName(itemSalt, "Salt");
+	
+	LanguageRegistry.addName(accessoryChiliSpice, "Chili Spice");
+	LanguageRegistry.addName(accessoryOnionSpice, "Onion Spice");
+	LanguageRegistry.addName(accessoryBeefSpice, "Beef Spice");
+	LanguageRegistry.addName(accessoryCheeseSlice, "Cheese Slice");
+	LanguageRegistry.addName(accessoryButter, "Butter");
+	
+	LanguageRegistry.addName(drinkRedWine, "Red Wine");
+	LanguageRegistry.addName(drinkWhiteWine, "White Wine");
 	
 	// TODO: Cooking the milk so that cheese is created.
 	GameRegistry.addSmelting(3338, new ItemStack(Item.bucketMilk), 0.1f);
@@ -98,10 +145,14 @@ public class MoKitchen {
 	GameRegistry.addShapelessRecipe(new ItemStack(eatableButteredSandwich), new ItemStack(eatableSandwich), new ItemStack(accessoryButter));
 	
 	// TODO: Create a sandwich with cheese on it.
-	GameRegistry.addShapelessRecipe(new ItemStack(eatableCheeseSandwich), new ItemStack(eatableButteredSandwich), new ItemStack(itemCheese));
+	GameRegistry.addShapelessRecipe(new ItemStack(eatableCheeseSandwich), new ItemStack(eatableButteredSandwich), new ItemStack(accessoryCheeseSlice));
 	
 	// TODO: Create a sandwich with cheese and meat on it.
 	GameRegistry.addShapelessRecipe(new ItemStack(eatableMeatAndCheeseSandwich), new ItemStack(eatableCheeseSandwich), new ItemStack(itemMeat));
+	
+	// TODO: Create a sandwich with meat on it.
+	GameRegistry.addShapelessRecipe(new ItemStack(eatableMeatSandwich), new ItemStack(eatableButteredSandwich), new ItemStack(itemMeat));
+	
 	
 	}
 
